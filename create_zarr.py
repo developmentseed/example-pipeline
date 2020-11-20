ec43e6eefd972d1a830a74c8dfe9f0605e0e8279
Aimee Barciauskas <aimee@developmentseed.org>
2020-11-20 13:14:16 -0800

    Remove obsolete arg

--- create_zarr.py
+++ create_zarr.py
@@ -4,7 +4,7 @@ import xarray as xr
 source_dir = 'cache'
 target_zarr = 'target.zarr'
 files = [os.path.join(source_dir, f) for f in os.listdir(source_dir)]
-ds = xr.open_mfdataset(files, group='Grid', combine="by_coords", concat_dim='time')
+ds = xr.open_mfdataset(files, group='Grid', combine="by_coords")
 ds.to_zarr(target_zarr)
 
 dsz = xr.open_zarr(target_zarr)
